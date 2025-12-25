7bd99b21499129c3807669ac6148059c6aaa8e13
Nguyen Thai Binh <nguyen.tb@u.nus.edu>
2025-12-25 20:03:30 +0700

    Add V1 router scaffolding

--- backend/internal/app/app.go
+++ backend/internal/app/app.go
@@ -3,16 +3,19 @@ package app
 import (
 	"log/slog"
 
+	gin_jwt "github.com/appleboy/gin-jwt/v3"
 	"github.com/gin-gonic/gin"
 	"github.com/nutabi/cvwo-assignment/backend/internal/config"
+	v1 "github.com/nutabi/cvwo-assignment/backend/internal/handlers/v1"
 	"github.com/nutabi/cvwo-assignment/backend/internal/middleware"
 	"github.com/nutabi/cvwo-assignment/backend/internal/repository"
 	"gorm.io/driver/sqlite"
 )
 
 type App struct {
+	serverAddress  string
 	repo           repository.Repository
-	authMiddleware gin.HandlerFunc
+	authMiddleware *gin_jwt.GinJWTMiddleware
 }
 
 func Initialise(cfg config.Config) App {
@@ -30,13 +33,19 @@ func Initialise(cfg config.Config) App {
 		cfg.GetServerHostname(),
 		cfg.GetJWTSecretKey(),
 	)
-	return App{repo: repo, authMiddleware: auth.MiddlewareFunc()}
+	return App{repo: repo, authMiddleware: auth}
 }
 
 func (a *App) Start() {
-	// TODO: Initilise router
+	// Initialise router
+	r := gin.New()
 
-	// TODO: Add routes
+	// Add routes
+	v1.RegisterRoutes(r.Group("/v1"), a.repo, a.authMiddleware)
 
-	// TODO: Start listening
+	// Start listening
+	err := r.Run(a.serverAddress)
+	if err != nil {
+		slog.Error("Failed to start server", "error", err)
+	}
 }
---
+++ backend/internal/handlers/v1/router.go
@@ -0,0 +1,25 @@
+package v1
+
+import (
+	gin_jwt "github.com/appleboy/gin-jwt/v3"
+	"github.com/gin-gonic/gin"
+	"github.com/nutabi/cvwo-assignment/backend/internal/repository"
+)
+
+type handlers struct {
+	repo           repository.Repository
+	authMiddleware *gin_jwt.GinJWTMiddleware
+}
+
+func RegisterRoutes(
+	r *gin.RouterGroup,
+	repo repository.Repository,
+	authMiddleware *gin_jwt.GinJWTMiddleware,
+) {
+	h := handlers{
+		repo:           repo,
+		authMiddleware: authMiddleware,
+	}
+
+	h.registerAuthRoutes(r)
+}
