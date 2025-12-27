3227d800af19d7b04c82efc14538a574ebf275e3
Nguyen Thai Binh <nguyen.tb@u.nus.edu>
2025-12-27 10:40:50 +0700

    Implement service error handling logic

--- backend/internal/handlers/v1/common.go
+++ backend/internal/handlers/v1/common.go
@@ -1,9 +1,11 @@
 package v1
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/nutabi/cvwo-assignment/backend/internal/service"
 )
 
 func respondWithError(c *gin.Context, code int, message string) {
@@ -12,5 +14,16 @@ func respondWithError(c *gin.Context, code int, message string) {
 
 func handleServiceError(c *gin.Context, err error) {
 	// TODO: Implement error handling logic
-	respondWithError(c, http.StatusInternalServerError, err.Error())
+	if err != nil {
+		if errors.Is(err, service.ErrUserNotFound) {
+			respondWithError(c, http.StatusNotFound, err.Error())
+		} else if errors.Is(err, service.ErrUsernameTaken) {
+			respondWithError(c, http.StatusConflict, err.Error())
+		} else if errors.Is(err, service.ErrEmailInUse) {
+			respondWithError(c, http.StatusConflict, err.Error())
+		} else {
+			respondWithError(c, http.StatusInternalServerError, "Internal server error")
+		}
+		return
+	}
 }
