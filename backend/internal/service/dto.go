21a00e9d2a9f92df69284c71e731e5dab408ff1f
Nguyen Thai Binh <nguyen.tb@u.nus.edu>
2025-12-27 16:44:18 +0700

    Add DTOs for all objects

--- backend/internal/service/dto.go
+++ backend/internal/service/dto.go
@@ -10,3 +10,33 @@ type UserProfile struct {
 	AvatarUrl *string    `json:"avatar_url"`
 	Bio       *string    `json:"bio"`
 }
+
+type TopicInfo struct {
+	TopicID     uint        `json:"topic_id"`
+	CreatedAt   time.Time   `json:"created_at"`
+	UpdatedAt   time.Time   `json:"updated_at"`
+	Title       string      `json:"title"`
+	Description string      `json:"description"`
+	Author      UserProfile `json:"author"`
+	Posts       []PostInfo  `json:"posts,omitempty"`
+}
+
+type PostInfo struct {
+	PostID    uint          `json:"post_id"`
+	CreatedAt time.Time     `json:"created_at"`
+	UpdatedAt time.Time     `json:"updated_at"`
+	Title     string        `json:"title"`
+	Content   string        `json:"content"`
+	Author    *UserProfile  `json:"author,omitempty"`
+	Topic     *TopicInfo    `json:"topic,omitempty"`
+	Comments  []CommentInfo `json:"comments,omitempty"`
+}
+
+type CommentInfo struct {
+	CommentID uint         `json:"comment_id"`
+	CreatedAt time.Time    `json:"created_at"`
+	UpdatedAt time.Time    `json:"updated_at"`
+	Content   string       `json:"content"`
+	Author    *UserProfile `json:"author,omitempty"`
+	Post      *PostInfo    `json:"post,omitempty"`
+}
