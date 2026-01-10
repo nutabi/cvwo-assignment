73369206364a0880478d074000965a66b0ca26c0
Nguyen Thai Binh <nguyen.tb@u.nus.edu>
2026-01-10 17:01:44 +0800

    Install & Use Tailwind components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,7 @@
-import './App.css'
-
 function App() {
   return (
-    <div className="app">
-      <h1>Web Forum</h1>
+    <div className="max-w-7xl mx-auto p-4">
+      <h1 className="text-3xl font-bold">Web Forum</h1>
     </div>
   )
 }
